dd74ab2c76ac594d592d5793d1de18ee5f666ee8
VitaliyFeytser <vitaliyfeytser@gmail.com>
2019-04-18 21:26:37 -0700

    burgers show up on local server

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ("Hawaiian Burger");
+INSERT INTO burgers (burger_name) VALUES ("Big Texas Burger");
+INSERT INTO burgers (burger_name) VALUES ("Santa Fe Melted Burger");
+
+SELECT * FROM burgers;
