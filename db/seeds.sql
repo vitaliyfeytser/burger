1c934c922c230f8d06c3ffd45a7574c7e5dfeff4
VitaliyFeytser <vitaliyfeytser@gmail.com>
2019-04-19 21:05:56 -0700

    done

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,7 @@
 INSERT INTO burgers (burger_name) VALUES ("Hawaiian Burger");
 INSERT INTO burgers (burger_name, devoured) VALUES ("Texas BBQ Burger", true);
 INSERT INTO burgers (burger_name, devoured) VALUES ("Sloppy Joe Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Grassy Messy", true);
 INSERT INTO burgers (burger_name) VALUES ("Big Texas Burger");
 INSERT INTO burgers (burger_name) VALUES ("Santa Fe Melted Burger");
 
